92aa1316db57d8f7cc5212c1619fcfa27e903c5b
JoelHauser <joel.hauser@gmail.com>
2022-08-12 23:47:01 -0400

    updatE

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Feature from "./components/Feature";
@@ -6,18 +6,28 @@ import Feature from "./components/Feature";
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
+  position: relative;
+`;
+const Shape = css `
+position: absolute;
+   width: 100%;
+   height: 100%;
+   top: 0;
+   left: 0;
+   z-index: -1;
 `;
 
-// const IntroShape = styled.div`
-//   clip-path: polygon(100% 0%, 61% 0%, 33% 100%, 100% 100%);
-//   background-color: crimson;
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   top: 0;
-//   left: 0;
-//   z-index: -1;
-// `;
+const IntroShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0, 100% 100%, 55% 100%);
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0, 33% 100%, 0 100%);
+  
+`;
+
 
 const App = () => {
   return (
@@ -25,10 +35,11 @@ const App = () => {
       <Container>
         <Navbar />
         <Intro />
-        {/* <IntroShape /> */}
+        <IntroShape />
       </Container>
       <Container>
         <Feature />
+        <FeatureShape />
       </Container>
     </>
   );
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,32 +1,119 @@
-import React from 'react'
-import styled from 'styled-components'
-import BroadOcean from "../img/BroadOcean.jpg"
-
+import React from "react";
+import styled from "styled-components";
+import BroadOcean from "../img/BroadOcean.jpg";
 
 const Container = styled.div`
-display: flex;
-
-`
+  display: flex;
+`;
 const Left = styled.div`
-    width: 50%;
-`
+  width: 50%;
+`;
 
 const Right = styled.div`
-width: 50%;
-`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const Image = styled.img`
+  width: 80%;
+  clip-path: polygon(0 0, 50% 0, 33% 100%, 0 100%);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
 
-`
-
+const Title = styled.span`
+  font-size: 70px;
+`;
+const SubTitle = styled.span`
+  font-size: 24px;
+  font-style: italic;
+  color: #333;
+  margin-top: 30px;
+`;
+const List = styled.li`
+  font-weight: bold;
+`;
+const We = styled.p`
+  font-size: 20px;
+  color: black;
+  padding: 10px;
+`;
+const Decs = styled.p`
+  font-style: 20px;
+  color: #777;
+  margin-top: 30px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Button = styled.button`
+  width: 150px;
+  border: none;
+  padding: 15px 20px;
+  background-color: red;
+  color: white;
+  font-size: 20px;
+  border-radius: 20px;
+  cursor: pointer;
+  margin-top: 20px;
+`;
 
 const Feature = () => {
   return (
     <Container>
-        <Left><Image src={BroadOcean}/></Left>
-        <Right></Right>
-        </Container>
-  )
-}
+      <Left>
+        <Image src={BroadOcean} />
+      </Left>
+      <Right>
+        <Title>
+          <b>Our</b> Mission,
+          <br />
+          Maintaining the highest quality standard.
+        </Title>
+        <SubTitle>What makes us great?</SubTitle>
+        <Decs>
+          Building a strong and trusting relationship with our clients is very
+          important to us. We believe in the old fashion way of business, client
+          first. We strive to maintain well trained, honest, trustworthy,
+          reliable and very personable staff. Our focus and drive is customer
+          satisfaction, this is our motivation. We believe in delivering a
+          service that will satisfy all our clients cleaning needs.
+        </Decs>
+        <Decs>
+          <We>
+            <i>
+              <b>We promise you this.</b>
+            </i>
+          </We>
+          <List>Superior service</List>
+          <List>It's about you and your needs</List>
+          <List>
+            We have a strict hiring practice and employee training which shows
+            in the integrity of our work
+          </List>
+          <List>
+            We are not looking to break the bank. Our rates are very competitive
+          </List>
+          <List>
+            In the event there is a client concern, we take it serious and
+            implement a corrective action to ensure the issue does not reoccur
+          </List>
+          <List>
+            We appreciate our opportunities. Because of this, we go over and
+            beyond to make sure our clients are 100% satisfied with Executive
+            Cleaning Solutions
+          </List>
+        </Decs>
+        <Button>Learn More</Button>
+      </Right>
+    </Container>
+  );
+};
 
-export default Feature
+export default Feature;
